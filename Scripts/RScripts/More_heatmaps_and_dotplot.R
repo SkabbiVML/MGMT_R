891931e4e9610b0dcfe67e276152c59767222a1c
SkabbiVML <61014551+SkabbiVML@users.noreply.github.com>
2023-05-02 16:08:39 +0200

    Heatmaps

--- Scripts/RScripts/More_heatmaps_and_dotplot.R
+++ Scripts/RScripts/More_heatmaps_and_dotplot.R
@@ -7,7 +7,7 @@ library(RColorBrewer)
 
 
 ##### Removing DenStem Samples
-Metsum_ROI_long_DENREMOVE <- Metsum_ROI_long[c(2:128),]
+Metsum_ROI_long_DENREMOVE <- Metsum_ROI_long[c(2:129),]
 
 res <- pheatmap(Metsum_ROI_long_DENREMOVE,
                 cluster_rows = T,
@@ -35,7 +35,7 @@ res <- pheatmap(Metsum_ROI_long_DENREMOVE,
 
 ##### Only GBMs
 
-GBMs <- MGMT_RunSum %>% dplyr::select(SampleID,Diagnosis,Series) %>% filter(Diagnosis == "Glioblastoma") %>% distinct()
+GBMs <- MGMT_RunSum %>% dplyr::select(SampleID,Diagnosis,Series) %>% filter(Diagnosis == "Glioblastoma" & Series != "DenStem") %>% distinct()
 
 Metsum_ROI_long_GBM <- na.omit(Metsum_ROI_long[GBMs$SampleID,])
 
@@ -45,7 +45,7 @@ res <- pheatmap(Metsum_ROI_long_GBM,
                 cluster_rows = T,
                 cluster_cols = F,
                 clustering_method = "ward.D",
-                cutree_rows = 2,
+                cutree_rows = 3,
                 treeheight_row = 100,
                 border_color = NA,
                 scale = "none",
@@ -64,18 +64,59 @@ res <- pheatmap(Metsum_ROI_long_GBM,
                 legend_labels = c("0%","10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%")
 )
 
-X <- colSums(Metsum_ROI_long)/145
 
-X2 <- rbind(colnames(Metsum_ROI_long),X)
+res <-  pheatmap(Metsum_ROI_long_GBM,
+         cluster_rows = T,
+         cluster_cols = F,
+         clustering_method = "ward.D",
+         cutree_rows = 3,
+         treeheight_row = 100,
+         border_color = NA,
+         scale = "none",
+         kmeans_k = 4,
+         drop_levels = F,
+         color = rev(brewer.pal(n = 10, name = "Spectral")),
+         fontsize_col = 8,
+         fontsize_row = 4,
+         fontsize = 14,
+         legend = T,
+         show_rownames = F,
+         legend_breaks = seq(0,100,10),
+         annotation_col = col_anno,
+        # annotation_row = row_anno_GBM,
+         annotation_colors = ann_colors,
+         # main = "All samples,  (n=148)",
+         legend_labels = c("0%","10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%")
+)
 
-X3 <- as.data.frame(t(X2))
 
-names(X3) <- c("CpG", "Averag_Meth")
+res$kmeans
 
-X3$Averag_Meth <- as.numeric(X3$Averag_Meth)
-X3$CpG <- factor(X3$CpG, levels = unique(X3$CpG))
-X3$CpG <- as.integer(X3$CpG)
+#################### Dotplot of average methylation per CpG ######################
 
-ggplot(X3,aes(x=CpG,y=Averag_Meth))+
-  geom_point()+
-  geom_line()
+x <- Metsum_ROI_long[c(2:129),]
+
+colnames(x) <- c(-7:-1, 1:108)
+
+x$SampleID <- rownames(x)
+
+y2 <- MGMT_RunSum %>% dplyr::select(SampleID,Known_status)
+
+x2 <- left_join(x, y2) %>% na.omit() %>% gather(key = "CpG", value = "percentMeth", -c(Known_status,SampleID))
+
+x3 <- x2 %>% group_by(Known_status, CpG) %>% summarise(Average_Methylation = mean(percentMeth), SD_Methylation = sd(percentMeth))
+
+
+x3$CpG <- as.integer(x3$CpG)
+
+ggplot(x3, aes(x=CpG, y=Average_Methylation, group =  Known_status))+
+  geom_point(aes(color=Known_status), position = position_dodge(width = 0.9), size = 2)+
+#  guides(color = guide_legend(override.aes = list(size=2)))+
+  geom_linerange(aes(ymin=Average_Methylation-SD_Methylation, ymax=Average_Methylation+SD_Methylation),position = position_dodge(width = 0.9))+
+  scale_color_brewer(palette = "Set1")+
+  geom_line(aes(color=Known_status))+
+  geom_smooth(aes(color=Known_status), show.legend = FALSE)+
+  theme_bw()
+
+############################################### Dendrogram
+
